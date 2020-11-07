9a5fd21853f30d225aa31abb4055ca4fea461d30
Sureshvarma4599 <sureshvarma.n666@gmail.com>
2020-11-07 12:51:31 +0530

    prograd

---
+++ app.js
@@ -0,0 +1,56 @@
+setInterval(displayclock,500)
+function displayclock(){
+  var time= new Date();
+  var hrs=time.getHours();
+  var min=time.getMinutes();
+  
+
+  if(hrs>12){
+    hrs=hrs-12;
+  }
+  if(hrs==0){
+    hrs=12;
+  }
+  if(hrs<10){
+    hrs="0"+hrs;
+  }
+  if(min<10){
+    min="0"+min
+  }
+  document.getElementById("demo").innerHTML=hrs +" : "+ min;
+  document.getElementById("demo1").innerHTML=hrs +" : "+ min;
+
+}
+function myMusic(){
+    document.getElementById("songs").style.display='block';
+    document.getElementById("demo").style.display='none';
+}
+function s1(){
+    var sng1=document.getElementById("song1");
+    sng1.play();
+    
+    sng1.style.display="block";
+    document.getElementById("song2").pause();
+    document.getElementById("song2").style.display="none";
+    document.getElementById("song3").pause();
+    document.getElementById("song3").style.display="none";
+
+}
+
+function s2(){
+   document.getElementById("song1").pause();
+   document.getElementById("song1").style.display="none";
+   document.getElementById("song2").play();
+   document.getElementById("song2").style.display="block";
+   document.getElementById("song3").pause();
+   document.getElementById("song3").style.display="none";
+   
+}
+function s3(){
+    document.getElementById("song1").pause();
+    document.getElementById("song1").style.display="none";
+    document.getElementById("song2").pause();
+    document.getElementById("song2").style.display="none";
+    document.getElementById("song3").play();
+    document.getElementById("song3").style.display="block";
+}
